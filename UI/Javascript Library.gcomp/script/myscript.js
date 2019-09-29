197bc617cb859df100adcfa38528138e486add47
alien-wxr-temp <49577496+alien-wxr-temp@users.noreply.github.com>
2019-09-29 15:24:18 +0800

    create JS lib for NXG

---
+++ UI/Javascript Library.gcomp/script/myscript.js
@@ -0,0 +1,7 @@
+(function () {
+    // Use strict prevents silent and common JavaScript errors.
+    'use strict';
+    function barCSS() {
+        div.style.--ni-fill-background = '#EE1C25';
+    };
+}());
