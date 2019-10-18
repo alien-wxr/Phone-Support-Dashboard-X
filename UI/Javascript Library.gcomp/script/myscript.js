d1ceeab7b40df24e9ea7acb06303ae971c11b334
alien-wxr-temp <49577496+alien-wxr-temp@users.noreply.github.com>
2019-10-18 18:03:09 +0800

    remove js part

--- UI/Javascript Library.gcomp/script/myscript.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    // Use strict prevents silent and common JavaScript errors.
-    'use strict';
-    function barCSS() {
-        '37'.style.cssText='--ni-fill-background : #EE1C25';
-    };
-}());
