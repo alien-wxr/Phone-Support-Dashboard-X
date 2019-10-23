f241efb77561dc3b2b9c828118fcd893515853ef
alien-wxr-temp <49577496+alien-wxr-temp@users.noreply.github.com>
2019-10-23 16:30:11 +0800

    change to deliver waitNum

--- Data/getCallinQueue.py
+++ Data/getCallinQueue.py
@@ -16,23 +16,14 @@ import spider
 from bs4 import BeautifulSoup
 import operator
 
-soup = spider.spider('./Data/url2.txt')
+def get():
 
-xml = []
+    soup = spider.spider('./Data/url2.txt')
 
-for row_tag in soup.report.children:
-    l = []
-    for col_tag in row_tag.children:
-        if (len(col_tag.contents) != 0):
-            l.append(col_tag.contents[0])
-        else:
-            l.append('NULL')
-    xml.append(l)
-
-#xml = [list(t) for t in set(tuple(_) for _ in xml)]
-#xml.sort(key=operator.itemgetter(3))
-
-for item in xml:
-    for item2 in item:
-        print(item2.ljust(20,' '), end='|')
-    print()
+    waitNum = 0
+    for row_tag in soup.report.children:
+        for col_tag in row_tag.children:
+            if col_tag['name']=='Call in Queue':
+                waitNum = waitNum+int(col_tag.contents[0])
+    
+    return waitNum
